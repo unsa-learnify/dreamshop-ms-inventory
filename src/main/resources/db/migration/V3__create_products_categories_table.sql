9bb23741be6f71888b54a7dfc80d38917c5ff450
ahincho <ahincho@unsa.edu.pe>
2024-11-25 00:42:56 -0500

    Development: Migrate from PostgreSQL to MySQL

--- src/main/resources/db/migration/V3__create_products_categories_table.sql
+++ src/main/resources/db/migration/V3__create_products_categories_table.sql
@@ -4,4 +4,4 @@ CREATE TABLE products_categories (
     PRIMARY KEY (product_id, category_id),
     CONSTRAINT fk_product FOREIGN KEY (product_id) REFERENCES products (id) ON DELETE CASCADE,
     CONSTRAINT fk_category FOREIGN KEY (category_id) REFERENCES categories (id) ON DELETE CASCADE
-);
+) ENGINE=InnoDB;
